89580a67aeee679368eb4c6b94c4f351dee50d01
DHARANI-GOPAL <140586371+DHARANI-GOPAL@users.noreply.github.com>
2023-10-17 10:40:05 +0530

    Update Simple.java

--- Simple.java
+++ Simple.java
@@ -1,8 +1,7 @@
 public class Simple{
     public static void main(String args[]){
          System.out.println(" HELLO WORLD");
-        System.out.println(" HELLO WORLD");
-        System.out.println(" HELLO WORLD");
+       
          
     }
 }
